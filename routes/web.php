6499fd48ba8363c540b79fb227c60d450f09534d
akmxlsz <akmal.rahimey@live.iium.edu.my>
2023-06-15 01:57:38 +0800

    qq

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\Review2Controller;
 */
 
 Route::get('/', function () {
-    return view('testingwelcome');
+    return view('welcome');
 });
 
 Route::get('/home', function () {
